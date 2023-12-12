3d8a0996dc783138b82db94bd241d6558573a63c
allanjchen <allanchen053@gmail.com>
2023-12-12 15:21:17 -0800

    tinekering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const containers = document.querySelectorAll('.page-section-sub');
-
 const options = {
   root: null,
   rootMargin: '0px',
@@ -38,3 +37,29 @@ document.addEventListener("DOMContentLoaded", function() {
       handleScroll(event);
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM Conetnt Loaded!");
+  fetch("src/project_cards.json") 
+      .then(response => response.json())
+      .then(data => { 
+          generateContentCards(data.project-cards);
+      })
+      .catch(error => console.error("Error fetching JSON:", error));
+});
+
+function generateContentCards(cards) {
+  const container = document.getElementById("project-cards");
+  cards.forEach(card => {
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+      cardElement.innerHTML = `
+          <h1>${card.title}</h1>
+          <p>${card.description}</p>
+          <img src="${card.imageUrl}" alt="${card.title}">
+          <a href="${card.link}" target="_blank">Read more</a>
+      `;
+      container.appendChild(cardElement);
+  });
+}
