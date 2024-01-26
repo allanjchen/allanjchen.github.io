a6c9f0d14b3719c72b87d1cc4b1333a23a08aed6
allanjchen <allanchen053@gmail.com>
2024-01-26 15:10:33 -0800

    tinkering

--- src/index.js
+++ src/index.js
@@ -38,57 +38,67 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-
-// Function to generate content cards with a specific order
-function generateContentCards(cards) {
-  const container = document.getElementById("project-cards");
-  // Check if 'container' is found in the document
-  if (container) {
-    // Check if 'cards' is defined and is an array
-    if (Array.isArray(cards) && cards.length > 0) {
-      // Sort the cards based on the 'order' property
-      cards.sort((a, b) => (a.order || Infinity) - (b.order || Infinity));
-      cards.forEach(card => {
-        // Exclude cards
-        if (card.exclude) {
-          return;
-        }
-        // Create a card element
-        const cardLinkElement = document.createElement("a");
-        cardLinkElement.classList.add("card-link");
-        cardLinkElement.href = card.link;
-
-        const cardElement = document.createElement("div");
-        cardElement.classList.add("card");
-
-        // Populate card content
-        cardElement.innerHTML = `
-          <h2>${card.title}</h2>
-          <p>${card.cardDescription}</p>
-          <img src="${card.cardImage}" alt="Image Not Found">
-        `;
-
-          // Append the card content to the link
-          cardLinkElement.innerHTML = cardElement.outerHTML;
-          // Append the link to the container
-          container.appendChild(cardLinkElement);
-      });
-    } else {
-      console.error("Error: 'cards' is not an array or is empty.");
-    }
-  } else {
-    console.error("Error: 'project-cards' not found in the document.");
-  }
-}
-
 // Fetch JSON data and generate content cards
 fetch("../src/projects.json")
   .then(response => response.json())
   .then(data => {
-    if (data.cards && data.cards.length > 0) {
-      generateContentCards(data.cards);
+    if (data.projects && data.projects.length > 0) {
+      generateProjects(data.projects);
     } else {
-      console.error("Error: 'cards' is not an array or is empty.");
+      console.error("Error: 'projects' is not an array or is empty.");
     }
   })
   .catch(error => console.error("Error fetching JSON:", error));
+
+// Function to generate content cards with a specific order
+function generateProjects(projects) {
+  const container_card = document.getElementById("project-cards");
+  const container_page = document.getElementById("project-page");
+  // Check if 'container' is found in the document
+  if (container_card || container_page) {
+    // Check if 'projects' is defined and is an array
+    if (Array.isArray(projects) && projects.length > 0) {
+      // Sort the projects based on the 'order' property
+      projects.sort((a, b) => (a.order || Infinity) - (b.order || Infinity));
+      projects.forEach(project => {
+        // Exclude projects
+        if (project.exclude) {
+          return;
+        }
+        if (container_card) {
+        // Create cards
+        const cardLinkElement = document.createElement("a");
+        cardLinkElement.classList.add("card-link");
+        cardLinkElement.href = project.card.link;
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        // Populate card content
+        cardElement.innerHTML = `
+          <h2>${project.title}</h2>
+          <p>${project.card.description}</p>
+          <img src="${project.card.image}" alt="Image Not Found">
+        `;
+        // Append the card content to the link
+        cardLinkElement.innerHTML = cardElement.outerHTML;
+        // Append the link to container_card
+        container_card.appendChild(cardLinkElement);
+        }
+        if (container_page) {
+        // Create pages
+        const pageElement = document.createElement("div");
+        pageElement.classList.add("page");
+        // Populate page content
+        pageElement.innerHTML = `
+        <img src="${project.page.image_main}" alt="Image Not Found" style="width:600px;height:600px;border-radius:10px;">
+          `;
+        // Append the container to container_page
+        container_page.appendChild(pageElement);
+        }
+      });
+    } else {
+      console.error("Error: 'projects' is not an array or is empty.");
+    }
+  } else {
+    console.error("Error: 'project-cards' or 'project-page' not found in the document.");
+  }
+}
